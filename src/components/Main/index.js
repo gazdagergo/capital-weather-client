7ea06d182698d042094311e494e1b7d65b6abc4a
Gergő Gazda <gergo.gazda@agl.group>
2019-06-05 05:40:41 +0200

    set up router

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
 import './styles.css';
 import MyCities from '../../screens/MyCities';
 
-function Main() {
-  return <MyCities />
-}
+const Main = () => (
+  <BrowserRouter>
+    <Route exact path="/" component={MyCities} />
+  </BrowserRouter>
+)
 
 export default Main;
