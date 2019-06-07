6fd883a44082f9d5d421ef6382aa05452cb088df
Gergő Gazda <gergo.gazda@agl.group>
2019-06-07 07:00:08 +0200

    get city info from server

--- src/containers/Page3Container.js
+++ src/containers/Page3Container.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import axios from 'axios';
+
 import Page3 from '../screens/Page3';
+
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 class Page3Container extends Component {
   static propTypes = {
     match: PropTypes.shape({
       params: PropTypes.shape({}),
     }),
+    dispatch: PropTypes.func.isRequired,
   }
   
   static defaultProps = {
     match: { params: null }
+  }
+
+  componentDidMount(){
+    const { match: { params: { cityName, countryCode } }} = this.props;
+    const { dispatch } = this.props;
+    axios.get(`${apiBaseUrl}city/${cityName}/${countryCode}`);
+    dispatch({ type: 'GET_CITY_INFO '})
   }
 
   render(){
